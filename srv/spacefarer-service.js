e6e0ccfc522539855debe4ad4819c966d9a3a9da
László Kocsis <laczi1997@gmail.com>
2026-03-13 12:44:52 +0100

    entity validation

--- srv/spacefarer-service.js
+++ srv/spacefarer-service.js
@@ -2,7 +2,15 @@ export default (srv) => {
 
     // spacefarer validation
     srv.before('CREATE', 'Spacefarers', async (/**@type Request*/ req) => {
+        const { data } = req;
 
+        if (data.stardustCollection < 0) {
+            return req.error(400, 'Stardust collection cannot be negative');
+        }
+
+        if (data.wormholeNavigationSkill < 0) {
+            return req.error(400, 'Wormhole navigation skill cannot be negative');
+        }
     });
 
     // send notification email after creating a new Spacefarer
